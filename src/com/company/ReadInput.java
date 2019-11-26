cc8d1dbea2fad431f7f2f7a6feda5f43a80f3b29
Barjinder Paul Singh <37749971+barjinderpaul@users.noreply.github.com>
2019-11-26 12:38:39 +0530

    uncommented Filling of data in tables

--- src/com/company/ReadInput.java
+++ src/com/company/ReadInput.java
@@ -17,11 +17,11 @@ public class ReadInput {
 
         // creates and fills match table
         stmt.executeUpdate(CREATE_MATCH_TABLE_SQL);
-//        stmt.executeUpdate(FILL_MATCH_DATA);
+        stmt.executeUpdate(FILL_MATCH_DATA);
 
         // creates and fills delivery table
         stmt.executeUpdate(CREATE_DELIVERY_TABLE_SQL);
-//        stmt.executeUpdate(FILL_DELIVERY_DATA);
+        stmt.executeUpdate(FILL_DELIVERY_DATA);
 
         stmt.close();
         conn.close();
